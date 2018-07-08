bc4fbcf777f4001f009026cb4aee61f20fc718fd
Zablah <Zablah@MacBook-Pro-de-Zablah.local>
2018-07-08 18:17:22 -0500

    Added Header Bar

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import {
   View,
   Image
 } from 'react-native';
+import { Header } from 'react-native-elements';
 import Hub from './src/Hub';
 import RiegoTerreno from './src/img/RiegoTerreno.png';
 
@@ -10,19 +11,24 @@ export default class App extends Component {
   render() {
     //console.ignoredYellowBox = ['Remote debugger'];
     return (
-      <View style={styles.container}>
-        <View style={{ flex: 5 }}>
-          <Hub />
-        </View>
-
-        <View style={{ backgroundColor: '#2980b9', flex: 6 }}>
-          <Image
-            source={RiegoTerreno}
-            style={{ flex: 1, height: undefined, width: undefined }}
-            resizeMode='stretch'
+        <View style={styles.container}>
+          <Header
+            statusBarProps={{ barStyle: 'light-content' }}
+            outerContainerStyles={{ borderBottomWidth: 0 }}
+            centerComponent={{ text: 'Riego', style: { color: '#fff', fontSize: 24 } }}
           />
+          <View style={{ flex: 1, padding: 10 }}>
+            <Hub />
+          </View>
+
+          <View style={{ backgroundColor: '#2980b9', flex: 1 }}>
+            <Image
+              source={RiegoTerreno}
+              style={{ flex: 1, height: undefined, width: undefined }}
+              resizeMode='stretch'
+            />
+          </View>
         </View>
-      </View>
     );
   }
 }
@@ -31,7 +37,6 @@ const styles = {
   container: {
     flex: 1,
     flexDirection: 'column',
-    backgroundColor: '#2980b9',
-    padding: 10
+    backgroundColor: '#2980b9'
   }
 };
